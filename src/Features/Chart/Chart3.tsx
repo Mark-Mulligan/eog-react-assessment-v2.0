92fa6e6a65ded645bf268a281000ca17fe53e301
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-15 22:32:32 -0500

    formatted custom tool tip rendering

--- src/Features/Chart/Chart3.tsx
+++ src/Features/Chart/Chart3.tsx
@@ -16,28 +16,34 @@ const getSelectedMetrics = (state: IState) => {
 
 const convertToDate = (milliseconds: number) => {
   const date = new Date(milliseconds);
-  return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
-}
-
+  return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+};
 
 const CustomTooltip = ({ active, payload }: any) => {
-    if (active) {
-      return payload.map((item: any) => {
-        {console.log(item)}
-        return (
-        <div key={item.name} className="custom-tooltip border">
-        <h2>{item.name}</h2>
-        <p>{convertToDate(item.payload.at)}</p>
-        <p>{item.payload.value} {item.payload.unit}</p>
+  if (active) {
+    return (
+      <div className="custom-tooltip border">
+        <h6>{convertToDate(payload[0].payload.at)}</h6>
+        <div className="tooltip-readings">
+          {payload.map((item: any) => {
+            {
+              console.log(item);
+            }
+            return (
+              <div key={item.name}>
+                <p>
+                  {item.name}: {item.payload.value} {item.payload.unit}
+                </p>
+              </div>
+            );
+          })}
         </div>
-        )
-      })
-      
-    }
-    
-    return null;
-}
+      </div>
+    );
+  }
 
+  return null;
+};
 
 const chartColors = ['#E74C3C', '#566573', '#3498DB', '#58D68D', '#F4D03F', '#C39BD3'];
 
@@ -60,56 +66,60 @@ const Chart3 = ({ data }: ChartProps) => {
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="at" type="category" allowDuplicatedCategory={false} />
-        
-        {metrics.includes('injValveOpen') ? <YAxis yAxisId="percent" label="%" dataKey="value"/> : null}
-        {metrics.includes('tubingPressure') || metrics.includes('casingPressure') ? <YAxis yAxisId="psi" label="PSI" dataKey="value" /> : null}
-        {metrics.includes('oilTemp') || metrics.includes('waterTemp') || metrics.includes('flareTemp') ? <YAxis yAxisId="F" label="F" dataKey="value" /> : null}
 
-        <Tooltip content={<CustomTooltip/>}/>
+        {metrics.includes('injValveOpen') ? <YAxis yAxisId="percent" label="%" dataKey="value" /> : null}
+        {metrics.includes('tubingPressure') || metrics.includes('casingPressure') ? (
+          <YAxis yAxisId="psi" label="PSI" dataKey="value" />
+        ) : null}
+        {metrics.includes('oilTemp') || metrics.includes('waterTemp') || metrics.includes('flareTemp') ? (
+          <YAxis yAxisId="F" label="F" dataKey="value" />
+        ) : null}
+
+        <Tooltip content={<CustomTooltip />} />
         <Legend />
         {data.map((d: any, index: number) => {
           if (d.name === 'oilTemp' || d.name === 'waterTemp' || d.name === 'flareTemp') {
             return (
-                <Line
-                  isAnimationActive={false}
-                  yAxisId="F"
-                  dataKey="value"
-                  data={d.data}
-                  name={d.name}
-                  key={d.name}
-                  dot={false}
-                  stroke={chartColors[index]}
-                />
+              <Line
+                isAnimationActive={false}
+                yAxisId="F"
+                dataKey="value"
+                data={d.data}
+                name={d.name}
+                key={d.name}
+                dot={false}
+                stroke={chartColors[index]}
+              />
             );
           }
-          if (d.name=== "tubingPressure" || d.name === 'casingPressure') {
+          if (d.name === 'tubingPressure' || d.name === 'casingPressure') {
             return (
               <Line
-                  isAnimationActive={false}
-                  yAxisId="psi"
-                  dataKey="value"
-                  data={d.data}
-                  name={d.name}
-                  key={d.name}
-                  dot={false}
-                  stroke={chartColors[index]}
-                />
-            )
+                isAnimationActive={false}
+                yAxisId="psi"
+                dataKey="value"
+                data={d.data}
+                name={d.name}
+                key={d.name}
+                dot={false}
+                stroke={chartColors[index]}
+              />
+            );
           }
 
-          if (d.name=== "injValveOpen") {
+          if (d.name === 'injValveOpen') {
             return (
               <Line
-                  isAnimationActive={false}
-                  yAxisId="percent"
-                  dataKey="value"
-                  data={d.data}
-                  name={d.name}
-                  key={d.name}
-                  dot={false}
-                  stroke={chartColors[index]}
-                />
-            )
+                isAnimationActive={false}
+                yAxisId="percent"
+                dataKey="value"
+                data={d.data}
+                name={d.name}
+                key={d.name}
+                dot={false}
+                stroke={chartColors[index]}
+              />
+            );
           }
         })}
       </LineChart>
